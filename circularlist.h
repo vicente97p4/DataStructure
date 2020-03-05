6908da15053288ebc46bb3f9d5d912b1a10dc87e
vicente97p4 <59174085+vicente97p4@users.noreply.github.com>
2020-03-06 00:45:23 +0900

    circular_list

---
+++ circularlist.h
@@ -0,0 +1,27 @@
+#ifndef _CIRCULARLIST_
+#define _CIRCULARLIST_
+
+typedef struct CircularListNodeType {
+	int data;
+	struct CircularListNodeType* pLink;
+}CircularListNode;
+typedef struct CircularListType {
+	int currentElementCount;
+	CircularListNode *pLink;
+}CircularList;
+
+CircularList* createCircularList();
+void deleteCircularList(CircularList* pList);
+int addCLElement(CircularList* pList, int position, CircularListNode element);
+int removeCLElement(CircularList* pList, int position);
+void clearCircularList(CircularList* pList);
+int getCircularListLength(CircularList* pList);
+CircularListNode* getCLElement(CircularList* pLIst, int position);
+void displayCircularList(CircularList* pList);
+#endif
+
+#ifndef _COMMON_LIST_DEF_
+#define _COMMON_LIST_DEF_
+#define TRUE 1
+#define FALSE 0
+#endif
